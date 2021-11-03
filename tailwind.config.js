c8cf307feab51bf0cc8873629a470e602cef6dc7
IHunte <ihunte.dev@gmail.com>
2021-11-03 23:48:48 +0100

    add card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
     mode: "jit",
     purge: ["./src/index.html"],
     darkMode: false, // or 'media' or 'class'
-    theme: {},
+    theme: {
+      extend: {
+        colors: {
+          'body': '#1F0318',
+          'title': '#E5F2C9',
+          'card': '#6564DB',
+          'question': '#E5F2C9',
+          'items': '#E5F2C9',
+          'hover-item': '#EFBDEB',
+        },
+      },
+    },
     variants: {},
     plugins: [],
   };
