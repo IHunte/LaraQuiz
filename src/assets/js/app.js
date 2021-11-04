ae396b8cd48a2aef2429ab7c808a52efd0e2d245
IHunte <ihunte.dev@gmail.com>
2021-11-04 11:28:38 +0100

    add fetch q/a using vuejs

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,6 +3,19 @@ import Vue from 'vue'
 new Vue({
     el: '#vue-app',
     data: {
-        message: 'Vue 2'
+        currentQ: 0,
+        questions: [
+            {
+                q: 'What is the current version of Laravel ?',
+                answers: { a: '5', b: '6', c: '7', d: '8' },
+                correct_answer: 'd',
+            },
+        ],
     },
+    methods: {
+        Count_Questions() {
+            return this.questions.length;
+        },
+
+    }
 });
